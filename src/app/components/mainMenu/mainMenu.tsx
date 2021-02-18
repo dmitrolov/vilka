662971e9837a99cd260ce60189f93fd62e5f63bd
Vorlondo <380968085709d@gmail.com>
2021-02-18 19:20:02 +0200

    footer-done//mainManu-almost( there are some problems) зроблена одна версія і немає відступів між блок

--- src/app/components/mainMenu/mainMenu.tsx
+++ src/app/components/mainMenu/mainMenu.tsx
@@ -1,7 +1,32 @@
+import './mainMenu.scss';
 export const MainMenu: React.FC = () => {
     return (
-      <header>
-          MainMenu
-      </header>
+      <div className="mainMenu">
+          <div className="productCard">
+            <img className='productCard__img' src="productimg/product_1.jpg" alt="product"/>
+            <span className='productCard__desc'>
+              назва товару тут, марка товару, код товару 
+            </span>
+            <div className='productCard__estimation'>
+              <img className="productCard__svg" src="productimg/star.svg" alt="star"/>
+              <a href="#" className='estimation__reviews'>Відгуки</a>
+            </div>
+            <div className='productCard__wrap'>
+              <span className='productCard__priсe'>
+                цена в Грівнях
+              </span>
+              <div className='productCard__chosen'>
+                  <a href="#">
+                    <img className='productCard__svg' src="productimg/heart.svg" alt="<3"/>
+                  </a>
+                </div>
+                <div className='productCard__basket'>
+                  <a href="#">
+                    <img className='productCard__svg' src="productimg/basket.svg" alt="telega"/>
+                  </a>
+                </div>
+            </div>
+          </div>
+      </div>
     );
   }
